a29e8c78853ac278c96c4fddad870b11b506dbe7
WOUTER PIETERSMA <NOORDERPOORT@ST.NOORDERPOORT.NL>
2026-02-11 13:46:13 +0100

    personen component toegevoegd, bestuur pagina is nu functioneel. Zal mogelijk nog geoptimaliseerd moeten worden

--- src/pages/Bestuur.js
+++ src/pages/Bestuur.js
@@ -1,11 +1,21 @@
+import personen from "../components/personen.js";
 const Bestuur = () => {
     document.title = "Schrijvershoek || Bestuur";
     const root = document.getElementById('root');
     root.innerHTML = `
-    <div class="max-w-7xl mx-auto px-4 py-8">
-        <h1>Welkom bij de Schrijvershoek!</h1>
-        <p>Bestuur content</p>
+    <div class="bg-cyaan/75 py-40 rounded-b-3xl mb-8">
+        <div class="max-w-7xl mx-auto px-4 py-8 flex items-center justify-center">
+            <h1 class="text-4xl font-bold text-center text-gray-800 mb-6 my-auto">Schrijvershoek</h1>
+        </div>
     </div>
+    <div class="max-w-7xl mx-auto px-4 py-8">
+        <h2 class="text-3xl font-bold text-gray-800 mb-4 text-center">Bestuur</h2>
+        <div class="rounded-lg p-6 flex items-center justify-between mb-8 max-w-7xl">
+            ${ (() => { let result = ''; for(let i = 0; i <= 4; i++) { result += personen(); } return result; })() }
+        </div>
+
+        
+     </div>
     `;
 }
 
